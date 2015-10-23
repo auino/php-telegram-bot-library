ebf34450a77f492f77e3d7a722043b1257428694
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2015-10-23 09:10:52 +0200

    Update on install.php

--- install.php
+++ install.php
@@ -2,24 +2,34 @@
 // If needed, check the official project on GitHub:
 // https://github.com/auino/php-telegram-bot-library
 
+$DELETEEXISTENTWBHOOK = false;
 $REGISTERWEBHOOK = true;
 $TOKEN = "...";
 $SSLCERTIFICATEFILE = "@certificate.pem";
 $WEBHOOKURL = "https://www.yourwebsite.org/webhook.php";
 
 $SETUPDB = true;
+
 $SETUPDBQUERIES = [
 	"CREATE TABLE `Logs` (`id` bigint(20) NOT NULL AUTO_INCREMENT, `bot` varchar(100) NOT NULL, `action` varchar(100) NOT NULL, `chat` int(11) NOT NULL, `type` varchar(30) NOT NULL, `content` varchar(250) NOT NULL, `date` varchar(30) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `bot` (`bot`,`action`,`chat`,`date`));"
 ];
 
 include_once("lib/telegram.php");
 
-if($REGISTERWEBHOOK) {
-	echo "Registering webhook...\n";
+if($DELETEEXISTENTWBHOOK) {
+	echo "Deleting registered webhook...\n";
 	$bot = new telegram_bot($token);
-	//$bot->set_webhook();
-	$bot->set_webhook($WEBHOOKURL, $SSLCERTIFICATEFILE);
-	echo "Registered!\n";
+	$bot->set_webhook();
+	echo "Deleted!\n";
+}
+else { // you can register a new webhook only if you're not deleting existent webhook
+	if($REGISTERWEBHOOK) {
+		echo "Registering webhook...\n";
+		$bot = new telegram_bot($token);
+		//$bot->set_webhook();
+		$bot->set_webhook($WEBHOOKURL, $SSLCERTIFICATEFILE);
+		echo "Registered!\n";
+	}
 }
 
 if($SETUPDB) {
