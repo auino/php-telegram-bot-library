a2fb702569a7c4ec9c453da2175b7eabc9d7ff5f
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2015-11-11 08:53:08 +0100

    Small enhancements on database functions

--- lib/db.php
+++ lib/db.php
@@ -9,7 +9,7 @@ function db_close($conn) {
 	mysql_close($conn);
 }
 
-// db_connect() and db_close() executions are accomplished inside of this function
+// db_connect() and db_close() executions are implicitly accomplished inside of this function
 function db_nonquery($q) {
 	global $DB_NAME;
 	$conn = db_connect();
@@ -20,7 +20,7 @@ function db_nonquery($q) {
 	return $retval;
 }
 
-// db_connect() and db_close() executions are accomplished inside of this function
+// db_connect() and db_close() executions are implicitly accomplished inside of this function
 function db_query($q) {
 	global $DB_NAME;
 	$conn = db_connect();
@@ -28,13 +28,14 @@ function db_query($q) {
 	mysql_select_db($DB_NAME);
 	$retval = mysql_query($q, $conn);
 	if(!$retval) return false;
-	$res = array();
+	$res = array(); // this may not be needed, since $retval already is an array
 	while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) array_push($res, $row);
 	db_close($conn);
 	return $res;
 }
 
 // this function is useful on non performant devices to get a single random element without using the SQL RAND() function, loading results in memory
+// db_connect() and db_close() executions are implicitly accomplished inside of this function
 function db_randomone($table, $filter=null) {
 	if($filter != null) $filter = "WHERE $filter";
 	else $filter = "";
