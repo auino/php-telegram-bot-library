3aa8a01e872505c9df081a3e949a405d02817dfc
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2016-09-20 08:57:10 +0200

    Added mysqli support

--- lib/config.php
+++ lib/config.php
@@ -8,6 +8,8 @@ $DB_USER='telegram';
 $DB_PWD='my-secret-password';
 $DB_NAME='telegram';
 
+$DB_USEOLDLIB=true; // if true, MySQL library (deprecated) is used; if false, mysqli library is used
+
 $STATES_ENABLED=false;
 
 $LOGS_ENABLED=true;
--- lib/db.php
+++ lib/db.php
@@ -1,35 +1,51 @@
 <?php
 function db_connect() {
-	global $DB_PORT, $DB_HOST, $DB_USER, $DB_PWD;
-	$conn = mysql_connect("$DB_HOST:$DB_PORT", $DB_USER, $DB_PWD);
+	global $DB_PORT, $DB_HOST, $DB_USER, $DB_PWD, $DB_USEOLDLIB;
+	$conn = null;
+	if($DB_USEOLDLIB) $conn = mysql_connect("$DB_HOST:$DB_PORT", $DB_USER, $DB_PWD);
+	else $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PWD, $DB_NAME);
 	return $conn;
 }
 
 function db_close($conn) {
-	mysql_close($conn);
+	global $DB_USEOLDLIB;
+	if($DB_USEOLDLIB) mysql_close($conn);
+	else mysqli_close($conn);
 }
 
 // db_connect() and db_close() executions are implicitly accomplished inside of this function
 function db_nonquery($q) {
-	global $DB_NAME;
+	global $DB_NAME, $DB_USEOLDLIB;
 	$conn = db_connect();
 	if(!$conn) return false;
-	mysql_select_db($DB_NAME);
-	$retval = mysql_query($q, $conn);
+	if($DB_USEOLDLIB) {
+		mysql_select_db($DB_NAME);
+		$retval = mysql_query($q, $conn);
+	}
+	else {
+		$retval = mysqli_query($conn, $q);
+	}
 	db_close($conn);
 	return $retval;
 }
 
 // db_connect() and db_close() executions are implicitly accomplished inside of this function
 function db_query($q) {
-	global $DB_NAME;
+	global $DB_NAME, $DB_USEOLDLIB;
 	$conn = db_connect();
 	if(!$conn) return false;
-	mysql_select_db($DB_NAME);
-	$retval = mysql_query($q, $conn);
+	if($DB_USEOLDLIB) {
+		mysql_select_db($DB_NAME);
+		$retval = mysql_query($q, $conn);
+	}
+	else {
+		$retval = mysqli_query($conn, $q);
+	}
 	if(!$retval) return false;
 	$res = array(); // this may not be needed, since $retval already is an array
-	while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) array_push($res, $row);
+	$row = null;
+	if($DB_USEOLDLIB) while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) array_push($res, $row);
+	else while($row = mysqli_fetch_array($retval, MYSQL_ASSOC)) array_push($res, $row);
 	db_close($conn);
 	return $res;
 }
